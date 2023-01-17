fbbd64cb4ca59f9b8bdbe4e7e0fbae08092c0f59
Fabricio-191 <fabricio19110@gmail.com>
2023-01-16 22:59:13 -0300

    made debug log save on process exit, removed address from base data

--- src/Base/debugger.ts
+++ src/Base/debugger.ts
@@ -8,12 +8,12 @@ let debugFile = 'debug.log';
 export function debug(data: BaseData | number | object | string, string: string, buffer?: Buffer): void {
 	if(!isEnabled) return;
 
-	if(typeof data === 'object' && 'address' in data){
+	if(typeof data === 'object' && 'ip' in data){
 		const type =
 			// eslint-disable-next-line no-nested-ternary
 			'multiPacketGoldSource' in data ? 'Server' : 'region' in data ? 'MasterServer' : 'RCON';
 
-		log += `[${type}] ${data.address} - ${string} `;
+		log += `[${type}] ${data.ip}:${data.port} - ${string} `;
 
 		if(buffer){
 			const parts = buffer.toString('hex').match(/../g) ?? [ '<empty>' ];
@@ -37,8 +37,25 @@ debug.enable = function(file = 'debug.log'): void {
 	isEnabled = true;
 	debugFile = file;
 
+
 	setInterval(debug.save, 1000).unref();
-	process.on('beforeExit', () => debug.save());
+
+	const handleExit = (): void => {
+		debug.save();
+
+		setTimeout(() => {
+			debug.save();
+			// eslint-disable-next-line no-console
+			console.log('debugging end');
+			process.exit();
+		}, 5000);
+	};
+
+	process.on('beforeExit', handleExit);
+	process.on('SIGINT', handleExit);
+	process.on('SIGUSR1', handleExit);
+	process.on('SIGUSR2', handleExit);
+	process.on('uncaughtException', handleExit);
 };
 
 debug.save = function(file = debugFile): void {
